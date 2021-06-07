db28fe0745b03763de0f66452f2874f73dbb7f4d
johnand4 <johnand4@oregonstate.edu>
2021-06-06 22:05:28 -0700

    updated tests for division

--- test_calculator.py
+++ test_calculator.py
@@ -7,12 +7,16 @@ import calculator
 class TestCalculatorApp:
     def test_add(self):
         assert 5 == calculator.add(3, 2)
+        assert 12 == calculator.add(7, 5)
 
     def test_subtract(self):
         assert 5 == calculator.subtract(10, 5)
+        assert 3 == calculator.subtract(7, 4)
 
     def test_multiply(self):
         assert 4 == calculator.multiply(2, 2)
+        assert 100 == calculator.multiply(10, 10)
 
     def test_divide(self):
         assert 4 == calculator.divide(8, 2)
+        assert 2 == calculator.divide(34, 17)
